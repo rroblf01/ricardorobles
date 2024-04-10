6e189359856d7f5e2c8d67b8472d54a64118c22c
Ricardo Robles <rroblf01@estudiantes.unileon.es>
2024-04-11 00:13:27 +0200

    Improve deploy

--- deploy.ts
+++ deploy.ts
@@ -1,9 +1,13 @@
 import { serveDir } from "https://deno.land/std@0.217.0/http/file_server.ts";
 
-Deno.serve((req) => {
-  const pathname = new URL(req.url).pathname;
-    return serveDir(req, {
-        fsRoot: "dist/ricardo-portafolios/browser",
-      headers: ["Cache-Control: max-age=31536000"],
-    });
+Deno.serve(async (req) => {
+  const fsRoot = "dist/ricardo-portafolios/browser";
+  const headers = ["Cache-Control: max-age=31536000"];
+  const response = await serveDir(req, { fsRoot, headers });
+  if (response.status === 404) {
+    const url = new URL(req.url);
+      return Response.redirect(url.origin)
+  }
+  return response;
+  
 });
