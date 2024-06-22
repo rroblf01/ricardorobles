e30bcff92d9889e8860477bcfdac1875b8f1f89f
Ricardo Robles <rroblf01@estudiantes.unileon.es>
2024-06-22 14:28:58 +0200

    feat: Add source code links to project details

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -13,6 +13,7 @@ export class ProjectsComponent {
     title: string;
     description: string;
     imgPath: string;
+    sources: { url: string; text: string }[];
   }[] = [
     {
       url: "https://ricardo-password-manager.deno.dev/",
@@ -20,24 +21,37 @@ export class ProjectsComponent {
       description:
         "Un gestor de contraseñas creado con Angular y Hono. He usado JWT para la autenticación y la base de datos de Deno KV.",
       imgPath: "assets/password-manager.webp",
+      sources: [
+        {url: "https://github.com/rroblf01/ricardo-password-manager", text: "Front Code"},
+        {url: "https://github.com/rroblf01/ricardo-passwords", text: "Back Code"}
+      ]
     },
     {
       url: "https://ricardotypefast.deno.dev/",
       title: "Type Fast",
       description: "Una herramienta destinada a saber qué tan rápido eres escribiendo, está hecha con Angular.",
       imgPath: "assets/typefast.webp",
+      sources: [
+        {url: "https://github.com/rroblf01/typefast", text: "Code"},
+      ]
     },
     {
       url: "https://ricardo-wordle.vercel.app/",
       title: "Fast Wordle",
       description: "Un clon de Wordle hecho con FastAPI y HTML/CSS/JS vanilla.",
       imgPath: "assets/fastwordle.webp",
+      sources: [
+        {url: "https://github.com/rroblf01/fast-wordle", text: "Code"},
+      ]
     },
     {
       url: "https://ricardorobles.deno.dev/",
       title: "Portafolio",
       description: "Este mismo portafolio, para ello he usado Angular.",
       imgPath: "assets/portafolio.webp",
+      sources: [
+        {url: "https://github.com/rroblf01/ricardorobles", text: "Code"},
+      ]
     },
     {
       url: "https://ricardo-image-compressor.deno.dev/",
@@ -45,6 +59,9 @@ export class ProjectsComponent {
       description:
         "Un simple y rápido compresor de imágenes en línea hecho con Javascript vanilla.",
       imgPath: "assets/compress.webp",
+      sources: [
+        {url: "https://github.com/rroblf01/deno-compress", text: "Code"},
+      ]
     },
     {
       url: "https://ricardo-api-mock.deno.dev/",
@@ -52,6 +69,9 @@ export class ProjectsComponent {
       description:
         "Una herramienta para Mockear una API, para ello he usado Javascript y la base de datos de Deno KV.",
       imgPath: "assets/mock-api.webp",
+      sources: [
+        {url: "https://github.com/rroblf01/deno-mock-api", text: "Code"},
+      ]
     },
     {
       url: "https://ricardo-jsontointerface.deno.dev/",
@@ -59,6 +79,9 @@ export class ProjectsComponent {
       description:
         "Una herramienta para convertir un JSON en una interfaz de TypeScript, para ello he usado Javascript.",
       imgPath: "assets/json-interface.webp",
+      sources: [
+        {url: "https://github.com/rroblf01/deno-jsontointerface", text: "Code"},
+      ]
     },
   ];
 }
