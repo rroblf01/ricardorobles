4b8391debf63b0a117333e2fc0545bb7d5b30b9a
rroblf01 <ricardo.r.f@hotmail.com>
2024-07-26 11:06:27 +0200

    update all package version

--- deploy.ts
+++ deploy.ts
@@ -1,4 +1,4 @@
-import { serveDir } from "https://deno.land/std@0.217.0/http/file_server.ts";
+import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
 
 Deno.serve(async (req) => {
   const fsRoot = "dist/ricardo-portafolios/browser";
