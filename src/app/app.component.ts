5348a0bdcf345dc19aac8a9432033acd9a3d5694
Ricardo Robles <ricardo.r.f@hotmail.com>
2024-11-20 21:15:10 +0100

    optimize images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { AboutComponent } from "./components/about/about.component";
 import { ProjectsComponent } from "./components/projects/projects.component";
@@ -11,7 +10,6 @@ import { CertsComponent } from "./components/certs/certs.component";
 @Component({
     selector: "app-root",
     imports: [
-        RouterOutlet,
         NavbarComponent,
         AboutComponent,
         ProjectsComponent,
