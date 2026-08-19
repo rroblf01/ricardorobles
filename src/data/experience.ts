acce1827508bb4d2ddea4e4665110eae293eccb5
Ricardo Robles <ricardo.r.f@hotmail.com>
2026-08-19 12:23:07 +0200

    add new experience

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,5 +1,6 @@
 export const experience = [
-  { place: "QDQmedia", title: "Programador Back-end", start: "02/2022", end: "Actualidad" },
+  { place: "Paradigma Digital", title: "Programador Back-end", start: "06/2026", end: "Actualidad" },
+  { place: "QDQmedia", title: "Programador Back-end", start: "02/2022", end: "06/2026" },
   { place: "HP SCDS", title: "Ingeniero del Software", start: "06/2020", end: "02/2022" },
   { place: "Salvis", title: "Full-Stack", start: "10/2019", end: "01/2020" },
 ];
